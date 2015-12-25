a99239961509a5dc6591da4ebac24625700f916d
Cosmin Stroe <cstroe@backstopsolutions.com>
2015-12-25 01:57:26 -0600

    Parse CapitalOne CSVs into a LineItem.

--- lib/spendhawk.ex
+++ lib/spendhawk.ex
@@ -1,2 +1,21 @@
 defmodule SpendHawk do
+  def parse_capitalone_row(row) do
+    desc = Enum.at(row, 4)
+    debit = Enum.at(row, 5)
+    credit = Enum.at(row, 6)
+    cond do
+      debit != "" ->
+        {amt, _} = Float.parse(debit)
+        %LineItem{description: desc, amount: -1 * amt}
+      credit != "" ->
+        {amt, _} = Float.parse(credit)
+        %LineItem{description: desc, amount: amt}
+    end
+  end
+
+  def parse_capitalone_csv(file_name) do
+    File.stream!(file_name) |>
+    CSV.decode |>
+    parse_capitalone_row
+  end
 end
--- test/spendhawk_test.exs
+++ test/spendhawk_test.exs
@@ -2,7 +2,23 @@ defmodule SpendHawkTest do
   use ExUnit.Case
   doctest SpendHawk
 
+  import SpendHawk
+
   test "the truth" do
     assert 1 + 1 == 2
+  end
+
+  test "parsing a single capital one CSV debit row" do
+    debit = ["", "", "", "", "description", "12.05", ""]
+    %LineItem{description: desc, amount: amt} = parse_capitalone_row(debit)
+    assert desc == "description"
+    assert amt == -12.05
+  end
+
+  test "parsing a single capital one CSV credit row" do
+    credit = ["", "", "", "", "description", "", "11.04"]
+    %LineItem{description: desc, amount: amt} = parse_capitalone_row(credit)
+    assert desc == "description"
+    assert amt == 11.04
   end
 end
